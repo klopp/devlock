c89fe6b83cb0305550193f28e5857dac1579bddb
Vsevolod Lutovinov <v.lutovinov@digital-direct.ru>
2017-03-18 20:25:06 +0300

    rewritten

--- devlock.pl
+++ devlock.pl
@@ -1,145 +1,145 @@
 #!/usr/bin/perl
-
-###################################################################
-# Device Locker
-# Usage & features : https://sourceforge.net/p/devlock/wiki/Home/
-# Author e-mail    : klopp@yandex.ru
-###################################################################
+# -----------------------------------------------------------------------------
 use strict;
 use warnings;
-
 use Gtk2 '-init';
 use FindBin qw ( $RealBin );
 use constant ICON_PATH => $RealBin . '/i/';
 use constant APPNAME   => 'DevLocker';
-use constant APPVER    => '0.4';
+use constant APPVER    => '0.6';
 
-###################################################################
+# -----------------------------------------------------------------------------
 my $lockfile = $0;
 $0 =~ m|^.*/([^/]+)$| and $lockfile = $1;
-my $usage =
-  "Usage: $lockfile device_id [unlock]\nRun `xinput --list` to get device_id";
-_die( $usage ) if $#ARGV < 0 or $#ARGV > 1;
-_die( $usage ) unless $ARGV[0] =~ /^\d+$/;
-_die( $usage ) if $#ARGV == 1 and $ARGV[1] ne 'unlock';
-my $DEV_ID  = $ARGV[0];
-my $locked  = ( $#ARGV == 1 and $ARGV[1] eq 'unlock' );
-my $devname = `xinput --list --name-only $DEV_ID 2>&1`;
-chomp $devname if $devname;
-_die( "Can not get device name for ID '$DEV_ID'!" )
-  if !$devname
-  or $devname =~ /$DEV_ID$/;
-my $ICON_ON;
-my $ICON_OFF;
+my $usage = qq(
+ Usage:
+ $lockfile "DEVICE_ID_STRING [position|last|first]"
+ Default position is 'last'  
+ Run `xinput --list` to get DEVICE_ID_STRING );
 
-_loadIcons();
-$SIG{HUP} = sub {
-    _loadIcons( 1 );
-    $locked ^= 1;
-    _switchState();
-};
-
-$lockfile = "/var/lock/$lockfile.$DEV_ID.lock";
-if( open my $lock, q{<}, $lockfile )
-{
-    my $pid = <$lock>;
-    close $lock;
-    _die( "Already loaded for '$devname'!\n(device ID: $DEV_ID, PID: $pid)" )
-      if kill 0, $pid;
+_die($usage) if $#ARGV < 0 || $#ARGV > 1;
+my $position = 'last';
+if ( defined $ARGV[1] && $ARGV[1] =~ /^\d+|last|first$/ ) {
+    $position = $ARGV[1];
 }
-_die( "Can not create lock in '$lockfile'!" )
-  unless open my $lock, q{>}, $lockfile;
-print $lock $$;
-close $lock;
+elsif ( defined $ARGV[1] ) {
+    _die($usage);
+}
 
-###################################################################
+my ( $DEV_ID, $ICON_ON, $ICON_OFF ) = ( _get_dev_id() );
+_die($DEV_ID) unless $DEV_ID =~ /^\d+$/;
+_load_icons();
+
 my $trayicon = Gtk2::StatusIcon->new;
-$trayicon->set_tooltip(
-"$devname ($DEV_ID)\nLeft click: switch locking\nRight click: unlock and exit"
-);
-_switchState();
+my $locked   = 0;
+_trayicon_set_tooltip();
+_switch_state();
 $trayicon->signal_connect(
     'button_press_event' => sub {
         my ( undef, $event ) = @_;
-        if( $event->button eq 3 )
-        {
+        if ( $event->button eq 3 ) {
+            _switch_state() if $locked;
             Gtk2->main_quit;
         }
-        elsif( $event->button eq 1 )
-        {
-            _switchState();
+        elsif ( $event->button eq 1 ) {
+            _switch_state();
         }
         1;
     }
 );
 Gtk2->main;
-_unlock();
-unlink $lockfile;
 
-###################################################################
-sub _lock
+exit;
+
+# -----------------------------------------------------------------------------
+sub _trayicon_set_tooltip
 {
-    `xinput --disable $DEV_ID`;
+    $trayicon->set_tooltip(
+        "$ARGV[0] ($DEV_ID)\nLeft click: switch locking\nRight click: unlock and exit");
 }
 
-###################################################################
-sub _unlock
+# -----------------------------------------------------------------------------
+sub _get_dev_id
 {
-    `xinput --enable $DEV_ID`;
-}
-
-###################################################################
-sub _switchState
-{
-    if( $locked )
-    {
-        $trayicon->set_from_pixbuf( $ICON_ON );
-        _unlock();
+    my $x;
+    unless ( open( $x, '-|', 'xinput' ) ) {
+        return "Can't read xinput: $!\n";
     }
-    else
-    {
-        $trayicon->set_from_pixbuf( $ICON_OFF );
-        _lock();
+    my $pos = 0;
+    my $devstring;
+    while ( defined( my $line = <$x> ) ) {
+        chomp $line;
+        next unless $line =~ /$ARGV[0]/;
+        ++$pos;
+        if ( $position eq 'first' ) {
+            $devstring = $line;
+            last;
+        }
+        elsif ( $position eq 'last' ) {
+            $devstring = $line;
+        }
+        elsif ( $position == $pos ) {
+            $devstring = $line;
+            last;
+        }
     }
-    $locked ^= 1;
+    close $x;
+    unless ($devstring) {
+        return "Can't read \"$ARGV[0]\" from xinput at position '$position'\n";
+    }
+    unless ( $devstring =~ /id=(\d+)/ ) {
+        return "Can't find id=X in xinput output for \"$devstring\"\n";
+    }
+    return $1;
 }
 
-###################################################################
-sub _die
+# -----------------------------------------------------------------------------
+sub _load_icons
 {
-    my ( $msg, $nogtk ) = @_;
-
-    my $dialog = Gtk2::Dialog->new( APPNAME . ' v ' . APPVER,
-        undef, 'destroy-with-parent', 'gtk-ok' => 'none' );
-    my $label = Gtk2::Label->new( "\n$msg\n" );
-    $dialog->get_content_area()->add( $label );
-    $dialog->signal_connect( response => sub { Gtk2->main_quit } );
-    $dialog->show_all;
-    unless( $nogtk )
-    {
-        Gtk2->main;
-        exit;
-    }
-}
-
-###################################################################
-sub _loadIcons
-{
-    my ( $running ) = @_;
-
-    undef $ICON_ON  if $ICON_ON;
-    undef $ICON_OFF if $ICON_OFF;
-
     my $file_on  = "$DEV_ID-on.png";
     my $file_off = "$DEV_ID-off.png";
+
     $file_on  = 'dev-on.png'  unless -f ICON_PATH . $file_on;
     $file_off = 'dev-off.png' unless -f ICON_PATH . $file_off;
     $file_on  = ICON_PATH . $file_on;
     $file_off = ICON_PATH . $file_off;
-    eval { $ICON_ON = Gtk2::Gdk::Pixbuf->new_from_file( $file_on ); };
-    _die( "Error loading ON icon '$file_on'\n$@", $running ) if $@;
-    eval { $ICON_OFF = Gtk2::Gdk::Pixbuf->new_from_file( $file_off ); };
-    _die( "Error loading OFF icon '$file_off'\n$@", $running ) if $@;
+    eval { $ICON_ON = Gtk2::Gdk::Pixbuf->new_from_file($file_on); };
+    _die("Error loading ON icon '$file_on'\n$@") if $@;
+    eval { $ICON_OFF = Gtk2::Gdk::Pixbuf->new_from_file($file_off); };
+    _die("Error loading OFF icon '$file_off'\n$@") if $@;
 }
 
-###################################################################
+# -----------------------------------------------------------------------------
+sub _switch_state
+{
+    $DEV_ID = _get_dev_id();
+    return unless $DEV_ID;
+    _trayicon_set_tooltip();
+    if ($locked) {
+        $trayicon->set_from_pixbuf($ICON_ON);
+        `xinput enable $DEV_ID`;
+    }
+    else {
+        $trayicon->set_from_pixbuf($ICON_OFF);
+        `xinput disable $DEV_ID`;
+    }
+    $locked ^= 1;
+}
+
+# -----------------------------------------------------------------------------
+sub _die
+{
+    my ($msg) = @_;
+
+    my $dialog = Gtk2::Dialog->new( APPNAME . ' v ' . APPVER,
+        undef, 'destroy-with-parent', 'gtk-ok' => 'none' );
+    my $label = Gtk2::Label->new("\n$msg\n");
+    $dialog->get_content_area()->add($label);
+    $dialog->signal_connect( response => sub { Gtk2->main_quit } );
+    $dialog->show_all;
+    Gtk2->main;
+    exit;
+}
+
+# -----------------------------------------------------------------------------
+
